e78f8d3214f2e38df28a4dd1f40da6b9f8f82490
shehan_wilfred <shehanwilfred@gmail.com>
2018-05-06 03:35:19 +1000

    added QR code functionality and enhance some pages

---
+++ app/geolocation/scripts/IndexController.js
@@ -0,0 +1,10 @@
+
+var geolocation = angular.module('geolocation',[]);
+geolocation.factory('getLocation',function($scope, supersonic){
+	  return {
+        alertA: function() {
+            alert('a');
+        }    
+    };
+});
+
