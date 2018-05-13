1b315aa641b87fd0e9a03af6acab9e66be48d232
kulbeer singh <ss.kulbeer24@gmail.com>
2018-05-13 11:15:43 +1000

    Revert "Merge branch 'master' into Development"
    This reverts commit 361565d63d172d232bc8a209f35f69da9adeeaad, reversing
    changes made to 1d88413bfeaebfb9cd3c0dc041b68881c70fae2d.

--- app/modules/scripts/Home_PageController.js
+++ app/modules/scripts/Home_PageController.js
@@ -1,19 +1,16 @@
-
 angular
-	.module('modules')
+	.module("modules")
 	.controller("homepage_Controller", function($scope, supersonic){
 		supersonic.ui.drawers.disable(); 
 
+		// function to display the favourite baritsa of the user
+		// $scope.viewFavBaritsa=function(){
+		// 	window.open("https://www.google.com/maps/search/?api=1&query=-37.797708,144.968341&travelmode=driving&dir_action=navigate");
+		// };
 		$scope.viewFavBaritsa=function(latlong){
           var string="https://www.google.com/maps/search/?api=1&query="+latlong+"&travelmode=driving&dir_action=navigate";
           supersonic.logger.debug(string);
                     steroids.openURL(string);
-		};
-		
-		// function used to navigate user from home page to QR code generator page
-		$scope.goToScannerPage=function(){
-			supersonic.ui.drawers.disable();
-			supersonic.ui.layers.push("modules#Point_Scanner");
-		};
+        };
 
 	});
--- app/modules/scripts/Point_ScannerController.js
+++
@@ -1,8 +0,0 @@
-angular.module("modules")
-        .controller("pointscanner_Controller", function($scope, supersonic){
-            supersonic.ui.drawers.close();
-                // create new qr code and add data to it
-            new QRCode(document.getElementById("qrcode"), "User Id : test"); 
-             
-        });
-    
