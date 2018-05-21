d94c657b199c7e0fa674c70b332b68cb7081ee1e
shehan_wilfred <shehanwilfred@gmail.com>
2018-05-21 16:54:41 +1000

    added rules, enchanced UIs

--- app/modules/scripts/Home_PageController.js
+++ app/modules/scripts/Home_PageController.js
@@ -4,6 +4,7 @@ angular
 	.controller("homepage_Controller", function($scope, supersonic){
 		supersonic.ui.drawers.disable(); 
 
+		
 		$scope.viewFavBaritsa=function(latlong){
           var string="https://www.google.com/maps/search/?api=1&query="+latlong+"&travelmode=driving&dir_action=navigate";
           supersonic.logger.debug(string);
